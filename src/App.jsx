6e4be0e3ba325bad3b5c3ea8fad1e536ee1e1bcc
unknown <rayenjemmali05@gmail.com>
2025-02-09 00:07:05 +0000

    ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css'
 import Home from './pages/Home';
 import Header from './components/Header';
